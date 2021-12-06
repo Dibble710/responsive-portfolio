a57d5a3db1091ba8c6bfe3f774bf6f9ceded800b
Tyler Antoni <tylerantoni1025@icloud.com>
2021-12-06 10:29:18 -0500

    Worked On Services Section

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // Navigation Bar Effects On Scroll
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header')
-    header.classList.toggle('sticky', window.scrollY > 0);
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = ((modalClick) => {
+    serviceModals[modalClick].classList.add('active');
+})
+
+learnMoreBtns.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        modal(i);
+    })
+})
+
+modalCloseBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener('click', () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove('active')
+        })
+    })
+})
+
+// video url - https://youtu.be/IJzuN5UGqmo?t=3519
