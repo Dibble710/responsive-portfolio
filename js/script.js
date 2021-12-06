43072a996b4887f3494cc58bb418a837f5313da7
Tyler Antoni <tyler@dassle.us>
2021-12-06 17:38:00 -0500

    Starting Dark/Light Mode

--- js/script.js
+++ js/script.js
@@ -4,30 +4,28 @@ window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-
 // Service Modal
 const serviceModals = document.querySelectorAll(".service-modal");
 const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
 const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
 
-var modal = ((modalClick) => {
-    serviceModals[modalClick].classList.add('active');
-})
+var modal = (modalClick) => {
+  serviceModals[modalClick].classList.add("active");
+};
 
 learnMoreBtns.forEach((learnMoreBtn, i) => {
-    learnMoreBtn.addEventListener("click", () => {
-        modal(i);
-    })
-})
+  learnMoreBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
 
 modalCloseBtns.forEach((closeBtn) => {
-    closeBtn.addEventListener('click', () => {
-        serviceModals.forEach((modalView) => {
-            modalView.classList.remove('active')
-        })
-    })
-})
-
+  closeBtn.addEventListener("click", () => {
+    serviceModals.forEach((modalView) => {
+      modalView.classList.remove("active");
+    });
+  });
+});
 
 // Portfolio Modals
 
@@ -35,73 +33,97 @@ const portfolioModals = document.querySelectorAll(".portfolio-modal");
 const imgCards = document.querySelectorAll(".img-card");
 const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
 
-var portfolioModal = ((modalClick) => {
-    portfolioModals[modalClick].classList.add('active');
-})
+var portfolioModal = (modalClick) => {
+  portfolioModals[modalClick].classList.add("active");
+};
 
 imgCards.forEach((imgCard, i) => {
-    imgCard.addEventListener("click", () => {
-        portfolioModal(i);
-    })
-})
+  imgCard.addEventListener("click", () => {
+    portfolioModal(i);
+  });
+});
 
 portfolioCloseBtns.forEach((portfolioCloseBtn) => {
-    portfolioCloseBtn.addEventListener('click', () => {
-        portfolioModals.forEach((portfolioModalView) => {
-            portfolioModalView.classList.remove('active')
-        })
-    })
-})
+  portfolioCloseBtn.addEventListener("click", () => {
+    portfolioModals.forEach((portfolioModalView) => {
+      portfolioModalView.classList.remove("active");
+    });
+  });
+});
 
 // Stop modals from showing on page load
 
-
-// swiper 
+// swiper
 var swiper = new Swiper(".client-swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 // Scroll To Top Button
 
-const scrollToTopBtn = document.getElementById('scrollToTop');
+const scrollToTopBtn = document.getElementById("scrollToTop");
 
-window.addEventListener('scroll', () => {
-    scrollToTopBtn.classList.toggle('active', window.scrollY > 500);
-})
+window.addEventListener("scroll", () => {
+  scrollToTopBtn.classList.toggle("active", window.scrollY > 500);
+});
 
+scrollToTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
 
-scrollToTopBtn.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-})
+// Dark/Light theme
+
+const themeBtn = document.querySelector('.theme-btn')
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle('dark-theme');
+    themeBtn.classList.toggle('sun');
+
+    localStorage.setItem('saved-theme', getCurrentTheme());
+    localStorage.setItem('saved-icon', getCurrentIcon());
+
+});
+
+const getCurrentTheme = () => document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+const getCurrentIcon = () => themeBtn.classList.contains('sun') ? 'sun' : 'moon'
+
+const savedTheme = localStorage.getItem('saved-theme')
+const savedTheme = localStorage.getItem('saved-icon')
+
+if (savedTheme) {
+    document.body.classList[savedTheme === 'dark' ? 'add' : 'remove']('dark-theme')
+    themeBtn.classList[savedIcon === 'sun' ? 'add' : 'remove']('sun')
+}
 
 // Nav menu items active on page scroll
 
-window.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('section')
-    const scrollY = window.pageYOffset
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const scrollY = window.pageYOffset;
 
-    sections.forEach((section) => {
-        let sectionHeight = section.offsetHeight;
-        let sectionTop = section.offsetTop;
-        let id = section.getAttribute('id')
+  sections.forEach((section) => {
+    let sectionHeight = section.offsetHeight;
+    let sectionTop = section.offsetTop;
+    let id = section.getAttribute("id");
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector(".nav-items a[href*=" + id + "]").classList.add('active')
-        } else {
-            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove('active')
-        }
-    })
-})
-// video url - https://youtu.be/IJzuN5UGqmo?t=3519
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.remove("active");
+    }
+  });
+});
