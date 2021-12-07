6c6ece0cb9d1d37644c26a2538dabf29c623c0e5
Tyler Antoni <tyler@dassle.us>
2021-12-07 11:50:55 -0500

    UI finished

--- js/script.js
+++ js/script.js
@@ -83,26 +83,29 @@ scrollToTopBtn.addEventListener("click", () => {
 
 // Dark/Light theme
 
-const themeBtn = document.querySelector('.theme-btn')
+const themeBtn = document.querySelector(".theme-btn");
 
 themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle('dark-theme');
-    themeBtn.classList.toggle('sun');
+  document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("sun");
 
-    localStorage.setItem('saved-theme', getCurrentTheme());
-    localStorage.setItem('saved-icon', getCurrentIcon());
-
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrentIcon());
 });
 
-const getCurrentTheme = () => document.body.classList.contains('dark-theme') ? 'dark' : 'light';
-const getCurrentIcon = () => themeBtn.classList.contains('sun') ? 'sun' : 'moon'
+const getCurrentTheme = () =>
+  document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeBtn.classList.contains("sun") ? "sun" : "moon";
 
-const savedTheme = localStorage.getItem('saved-theme')
-const savedIcon = localStorage.getItem('saved-icon')
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
 
 if (savedTheme) {
-    document.body.classList[savedTheme === 'dark' ? 'add' : 'remove']('dark-theme')
-    themeBtn.classList[savedIcon === 'sun' ? 'add' : 'remove']('sun')
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"](
+    "dark-theme"
+  );
+  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
 }
 
 // Nav menu items active on page scroll
@@ -113,7 +116,7 @@ window.addEventListener("scroll", () => {
 
   sections.forEach((section) => {
     let sectionHeight = section.offsetHeight;
-    let sectionTop = section.offsetTop;
+    let sectionTop = section.offsetTop - 60;
     let id = section.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
@@ -127,3 +130,28 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+// Nav Menu Toggle
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a")
+
+menuBtn.addEventListener('click', () => {
+  navigation.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  navigation.classList.remove('active');
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navigation.classList.remove('active');
+  })
+})
+
+
+// Scroll Reveal Animations
+
